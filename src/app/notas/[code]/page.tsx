6c58367146b1f233cc5ded495e97ec89917c89fa
Daniel Haro Contreras <daniel@appfluence.com>
2023-12-20 22:33:32 +0100

    Replace 2nd instrument

--- src/app/notas/[code]/page.tsx
+++ src/app/notas/[code]/page.tsx
@@ -1,8 +1,9 @@
 import { parse } from "papaparse";
 import Link from "next/link";
 
-function showKey(key: string, showINS2: boolean) {
-  if (key.startsWith("INS2") && !showINS2) return false;
+function showKey(key: string, blacklist: string[]) {
+  if (blacklist.some((blacklistKey) => key.startsWith(blacklistKey)))
+    return false;
   if (["password", "Observaciones", "Boletín"].includes(key)) return false;
   return !key.includes("TRI");
 }
@@ -26,7 +27,7 @@ export default async function Notas({ params }: { params: { code: string } }) {
         <h1 className="text-4xl font-bold">Código inválido</h1>
         <Link
           className="transition-all rounded-xl bg-white text-black px-4 py-2 hover:bg-neutral-300"
-          href="/"
+          href="/notas/"
         >
           Volver
         </Link>
@@ -34,20 +35,33 @@ export default async function Notas({ params }: { params: { code: string } }) {
     );
   }
 
+  const keyBlacklist: string[] = [];
+
+  const instruments = row["Instrumento"].split("/");
+  const ins1 = instruments[0];
+  const ins2 = instruments[1];
+
+  const showINS1 = Object.keys(row).some(
+    (key) => key.startsWith("INS1") && row[key],
+  );
+  if (!showINS1) keyBlacklist.push("INS1");
   const showINS2 = Object.keys(row).some(
     (key) => key.startsWith("INS2") && row[key],
   );
+  if (!showINS2) keyBlacklist.push("INS2");
 
   return (
     <main className="flex flex-col items-center justify-between max-w-lg mx-auto px-4 mt-10 mb-4 m-auto">
       <table className="border">
         <tbody>
           {Object.keys(row)
-            .filter((key) => showKey(key, showINS2))
+            .filter((key) => showKey(key, keyBlacklist))
             .map((key) => (
               <tr key={key}>
                 <td className="p-2 font-bold self-end border">
-                  {key.replace("INS1", row["Instrumento"])}
+                  {key
+                    .replace("INS1", ins1)
+                    .replace("INS2", ins2 || "Segundo Instrumento")}
                 </td>
                 <td className="p-2 border">{row[key]}</td>
               </tr>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex-col flex w-full items-center mt-8 gap-4">
-      <h1>Escuela de Música y Danza de Padul</h1>
+    <main className="flex-col flex w-full items-center mt-8 gap-8">
+      <h1 className="text-2xl font-bold">Escuela de Música y Danza de Padul</h1>
       <Link
         href="/notas/"
         className="transition-all rounded-xl bg-white text-black px-4 py-2 hover:bg-neutral-300"
       >
-        Ver notas
+        Notas del primer trimestre
       </Link>
     </main>
   );
